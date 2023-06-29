cdfeaab1805424f6bb00ab8aec134c4389d27f5e
Gabriel Antunes <106894238+gabriellantunes@users.noreply.github.com>
2023-06-29 17:52:48 -0300

    Update jsInformacoes.js

--- HealthToday/js/jsInformacoes.js
+++ HealthToday/js/jsInformacoes.js
@@ -1,32 +1,4 @@
-/*window.onload = function () {
-    let currentUser = localStorage.getItem('currentUser');
-    if (!currentUser) {
-        window.location.href = 'error.html';
-        return;
-    }
 
-    let users = JSON.parse(localStorage.getItem('users')) || [];
-    let user = users.find(user => user.email === currentUser);
-
-    if (user) {
-        document.getElementById('welcome-message').textContent = `Olá, ${user.name}!`;
-
-        // Definir a imagem do usuário
-        let userPic = localStorage.getItem(currentUser + 'profilePic');
-        if (userPic) {
-            document.querySelector('.user-icon').src = userPic;
-            document.querySelector('#user-icon-menu').src = userPic;
-        }
-
-        if (user.feedbacks) {
-            for (let feedback of user.feedbacks) {
-                addFeedbackToPage(feedback);
-            }
-        }
-    } else {
-        window.location.href = 'login.html';
-    }
-};*/
 window.onload = function () {
     let currentUser = localStorage.getItem('currentUser');
     if (!currentUser) {
@@ -39,17 +11,13 @@ window.onload = function () {
 
     if (user) {
         document.getElementById('welcome-message').textContent = `Olá, ${user.name}!`;
-
-        // Definir a imagem do usuário
         let userPic = localStorage.getItem(currentUser + 'profilePic');
         if (userPic) {
             document.querySelector('.user-icon').src = userPic;
             document.querySelector('#user-icon-menu').src = userPic;
         }
 
-        // Mostrar o feedback atual do usuário
         if (user.feedback) {
-            // Limpeza da lista de feedbacks na página
             document.getElementById('feedback-list').innerHTML = '';
             addFeedbackToPage(user.feedback);
         }
@@ -92,43 +60,6 @@ function logout() {
     window.location.href = 'index.html';
 }
 
-/*
-document.getElementById('feedback-form').addEventListener('submit', function (event) {
-    event.preventDefault();
-
-    let currentUser = localStorage.getItem('currentUser');
-    if (!currentUser) {
-        alert('Por favor, faça login para deixar um feedback');
-        return;
-    }
-
-    let users = JSON.parse(localStorage.getItem('users')) || [];
-    let user = users.find(user => user.email === currentUser);
-    
-    let feedbackContent = document.getElementById('feedback-content').value;
-    let feedbackStars = document.querySelector('.rating').getAttribute('data-rating'); 
-
-    let feedback = {
-        content: feedbackContent,
-        stars: feedbackStars,
-        date: new Date()
-    };
-
-    if(!user.feedbacks) {
-        user.feedbacks = [];
-    }
-    user.feedbacks.push(feedback);
-    
-
-    localStorage.setItem('users', JSON.stringify(users));
-
-    // Adicionando feedback à página
-    addFeedbackToPage(feedback);
-
-    alert('Feedback enviado com sucesso!');
-});
-*/
-
 document.getElementById('feedback-form').addEventListener('submit', function (event) {
     event.preventDefault();
 
@@ -154,16 +85,12 @@ document.getElementById('feedback-form').addEventListener('submit', function (ev
 
     localStorage.setItem('users', JSON.stringify(users));
 
-    // Limpeza da lista de feedbacks na página
     document.getElementById('feedback-list').innerHTML = '';
 
-    // Adicionando feedback à página
     addFeedbackToPage(feedback);
 
     alert('Feedback enviado com sucesso!');
 });
-
-
 
 function getStarIcons(numStars) {
     let starHTML = '';
@@ -176,36 +103,12 @@ function getStarIcons(numStars) {
     return starHTML;
 }
 
-
-
-/*
 function addFeedbackToPage(feedback) {
     const feedbackList = document.getElementById('feedback-list');
     
     const feedbackContainer = document.createElement('div');
     feedbackContainer.classList.add('feedback-item');
     
-    const feedbackText = document.createElement('p');
-    feedbackText.textContent = feedback.content;
-    
-    const feedbackStars = document.createElement('div');
-    feedbackStars.innerHTML = getStarIcons(feedback.stars);
-    feedbackStars.classList.add('rating'); // Adicione esta linha
-    
-    feedbackContainer.appendChild(feedbackText);
-    feedbackContainer.appendChild(feedbackStars);
-    
-    feedbackList.appendChild(feedbackContainer);
-}*/
-
-
-function addFeedbackToPage(feedback) {
-    const feedbackList = document.getElementById('feedback-list');
-    
-    const feedbackContainer = document.createElement('div');
-    feedbackContainer.classList.add('feedback-item');
-    
-    // Adicionando a imagem do usuário e o nome
     const userImage = document.createElement('img');
     userImage.src = document.querySelector('.user-icon').src;
     userImage.style.width = '50px';
@@ -216,7 +119,7 @@ function addFeedbackToPage(feedback) {
 
     const userName = document.createElement('p');
     let name = document.getElementById('welcome-message').textContent.replace('Olá, ', '').replace('!', '');
-    name = name.charAt(0).toUpperCase() + name.slice(1);  // Aqui fazemos a primeira letra maiúscula
+    name = name.charAt(0).toUpperCase() + name.slice(1); 
     userName.textContent = name;
     userName.style.fontWeight = 'bold';
     userName.style.marginLeft = '60px';
@@ -224,13 +127,11 @@ function addFeedbackToPage(feedback) {
     feedbackContainer.appendChild(userImage);
     feedbackContainer.appendChild(userName);
     
-    // Adicionando o descritivo
     const feedbackText = document.createElement('p');
     feedbackText.textContent = feedback.content;
     feedbackText.classList.add('feedback-descritivo');
     feedbackContainer.appendChild(feedbackText);
 
-    // Adicionando as estrelas
     const feedbackStars = document.createElement('div');
     feedbackStars.innerHTML = getStarIcons(feedback.stars);
     feedbackStars.classList.add('rating');
